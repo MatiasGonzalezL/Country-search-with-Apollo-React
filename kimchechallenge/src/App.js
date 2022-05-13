e20b16645cc1a3062d2c3a639ce408c880d866bc
Matías Gonzalez <matias.alejandro.gonzalezl@gmail.com>
2022-05-12 23:52:08 -0400

    segundo commit

--- kimchechallenge/src/App.js
+++ kimchechallenge/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
@@ -9,19 +11,37 @@ const client = new ApolloClient({
 
 const App = () => (
   <ApolloProvider client={client}>
-    <div>
+    {/* <div>
       <h2>
         My first Apollo app{" "}
         <span role="img" aria-label="Rocket">
           🚀
         </span>
       </h2>
-      <h2>
-        My first Apollo app{" "}
-        <span role="img" aria-label="Rocket">
-          🚀
-        </span>
-      </h2>
+    </div> */}
+    <div className="div_principal">
+      <div className="div_titulo">
+        <h1>Country search</h1>
+        <h5>Some random text</h5>
+      </div>
+      <div>
+        <form className="input_general">
+          <label>
+            <span id="icon">
+              <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+            </span>
+            <input className="input_text" type="text" name="name" placeholder="Escribe el país a buscar" />
+          </label>
+        </form>
+      </div>
+      <div className="grupo">
+        <h2>Group by:</h2>
+        <button type="button" className="boton">Continent</button>
+        <button type="button" className="boton">Language</button>
+      </div>
+      <div>
+        <h2>Aquí van los países</h2>
+      </div>
     </div>
   </ApolloProvider>
 );
